def30b41884a52c98b21f827a5593c5a37c8cc2d
Himanshu Nishad <himanshunishadofficial@gmail.com>
2025-08-20 19:12:22 +0530

    UI change

--- src/App.jsx
+++ src/App.jsx
@@ -35,16 +35,19 @@ function App() {
 
   return (
     <>
-      <div className="relative isolate overflow-hidden bg-gray-900 py-10 sm:py-32">
-        <div className="w-full max-w-md mb-5 mx-auto shadow-md mt-0 rounded-lg p-2 text-emerald-50">
-          <h1 className="text-2xl font-mono font-semibold text-center text-stone-50 mb-4">
-            Password Generator
+      <div className="relative isolate overflow-hidden bg-gray-900 py-10 sm:py-20">
+        <div className="w-full max-w-md mx-auto shadow-md mt-0 rounded-lg p-2 text-emerald-50">
+          <h1 className="text-5xl font-sans font-semibold text-center text-stone-50 mb-4">
+            Password ⚡️ Shield
           </h1>
-          <div className="flex-shadow rounded-lg overflow-hidden mb-4 flex gap-1">
+          <p className="mb-4 text-lg text-center text-gray-300">
+            "Strong security starts with stronger passwords"{" "}
+          </p>
+          <div className="flex-shadow rounded-lg overflow-hidden mb-4 py-5 sm:py-5 flex gap-1">
             <input
               type="text"
               value={password}
-              className="outline-none w-full py-2 px-4 bg-white text-gray-800 rounded-lg"
+              className="outline-none w-screen py-2 px-4 bg-white text-gray-800 rounded-lg"
               placeholder="Password"
               readOnly
               ref={passwordReference}
@@ -61,8 +64,8 @@ function App() {
               {copied ? "Copied!" : "Copy"}
             </button>
           </div>
-          <div className="flex text-sm gap-x-2">
-            <div className="flex item-center gap-x-1">
+          <div className="flex text-sm gap-x-4">
+            <div className="flex item-center gap-x-2">
               <input
                 type="range"
                 className="cursor-pointer"
@@ -77,7 +80,7 @@ function App() {
               />
               <label htmlFor="numRange">Length:{length}</label>
             </div>
-            <div className="flex item-center gap-1">
+            <div className="flex item-center gap-2">
               <input
                 type="checkbox"
                 defaultChecked={numberAllowed}
@@ -126,134 +129,44 @@ function App() {
             className="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
           ></div>
         </div>
-        
       </div>
-      
-      <section className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
-  <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,var(--color-indigo-500),transparent)] opacity-10"></div>
-  <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-gray-900 shadow-xl ring-1 shadow-indigo-500/5 ring-white/5 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
-  <div className="mx-auto max-w-2xl lg:max-w-4xl">
-    <h2 className="text-2xl mb-4 font-semibold tracking-tight text-center text-white sm:text-7xl">
-              Brew Code Community
-            </h2>
-      <blockquote className="text-center text-xl/8 font-semibold text-white sm:text-2xl/9">
-        <p>“To be a global hub where developers of all levels collaborate, innovate, and grow together — fostering a culture of learning, contribution, and open-source excellence.”</p>
-      </blockquote>
-      <figcaption className="mt-10">
-        <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-          <div className="font-semibold text-white">Himanshu Nishad</div>
-          <svg viewBox="0 0 2 2" width="3" height="3" aria-hidden="true" className="fill-white">
-            <circle r="1" cx="1" cy="1" />
-          </svg>
-          <div className="text-gray-400">Founder of "Brew n Code"</div>
-        </div>
-      </figcaption>
-  
-  </div>
-</section>
 
- <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
-            <div className="max-w-xl lg:max-w-lg">
-              <h2 className="text-4xl font-semibold tracking-tight text-white">
-                Subscribe to our newsletter
-              </h2>
-              <p className="mt-4 text-lg text-gray-300">
-                ☕ Join the Brew Code Community! Subscribe to stay updated — code, learn, and grow together!
-              </p>
-              <div className="mt-6 flex max-w-md gap-x-4">
-                <label for="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  type="email"
-                  name="email"
-                  required
-                  placeholder="Enter your email"
-                  autocomplete="email"
-                  className="min-w-0 flex-auto rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                />
-                <button
-                  type="submit"
-                  className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                >
-                  Subscribe
-                </button>
-              </div>
+      <section className="relative isolate overflow-hidden bg-gray-900 px-6 py-10 sm:py-20   lg:px-8">
+        <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,var(--color-indigo-500),transparent)] opacity-10"></div>
+        <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-gray-900 shadow-xl ring-1 shadow-indigo-500/5 ring-white/5 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
+        <div className="mx-auto max-w-2xl lg:max-w-4xl">
+          <h2 className="text-2xl mb-8 font-semibold tracking-tight text-center text-white sm:text-7xl">
+            Brew 🅽 Code
+          </h2>
+          <blockquote className="text-center py-5 text-xl/8 font-light text-white sm:text-2xl/9">
+            <p>
+              "Place where an idea turned into a product"
+              </p> 
+              <p>Innovate, and grow
+              together — fostering a culture of learning, contribution, and
+              open-source excellence
+            </p>
+          </blockquote>
+          <figcaption className="mt-10">
+            <div className="mt-4 flex items-center text-2xl justify-center space-x-3 ">
+              <div className="font-bold  text-white">Himanshu Nishad</div>
+              <svg
+                viewBox="0 0 2 2"
+                width="3"
+                height="3"
+                aria-hidden="true"
+                className="fill-white"
+              >
+                <circle r="1" cx="1" cy="1" />
+              </svg>
+              <div className="text-gray-400">Founder of "Brew 🅽 Code"</div>
             </div>
-            <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
-              <div className="flex flex-col items-start">
-                <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    data-slot="icon"
-                    aria-hidden="true"
-                    className="size-6 text-white"
-                  >
-                    <path
-                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </div>
-                <dt className="mt-4 text-base font-semibold text-white">
-                  Weekly articles
-                </dt>
-                <dd className="mt-2 text-base/7 text-gray-400">
-                  Your weekly dose of code, ideas, and inspiration — delivered every week!
-                </dd>
-              </div>
-              <div className="flex flex-col items-start">
-                <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    dataSlot="icon"
-                    aria-hidden="true"
-                    className="size-6 text-white"
-                  >
-                    <path
-                      d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002"
-                      strokeLinecap="round"
-                      strokelinejoin="round"
-                    />
-                  </svg>
-                </div>
-                <dt className="mt-4 text-base font-semibold text-white">
-                  No spam
-                </dt>
-                <dd className="mt-2 text-base/7 text-gray-400">
-                  We respect your privacy - that’s our brew promise.
-                </dd>
-              </div>
-            </dl>
-          </div>
+          </figcaption>
         </div>
-        <div
-          aria-hidden="true"
-          className="absolute top-0 left-1/2 -z-10 -translate-x-1/2 blur-3xl xl:-top-6"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="aspect-1155/678 w-288.75 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-          ></div>
-        </div>
-      </div>
-
+      </section>
 
       <footer className="bg-black py-6 text-center text-gray-300">
-        &copy; {new Date().getFullYear()} MyPortfolio, All rights reserved.
+        &copy; {new Date().getFullYear()} HMNSD, All rights reserved.
       </footer>
     </>
   );
